9266e23a77b8c7765baec9d916c5cfd061767a62
ChristopheLE2 <van-minhchristophe.le@edu.esiee.fr>
2026-06-08 14:51:14 +0200

    change return response and add new descriptors

--- index_input/script/worker.py
+++ index_input/script/worker.py
@@ -8,12 +8,37 @@ INPUT_DIR = "/input"
 def extractReqFeatures(fileName, algo_choice):
     if fileName:
         img = cv2.imread(fileName)
+
+        if algo_choice==1: #Couleurs
+            algo_name = "BGR"
+            histB = cv2.calcHist([img],[0],None,[256],[0,256])
+            histG = cv2.calcHist([img],[1],None,[256],[0,256])
+            histR = cv2.calcHist([img],[2],None,[256],[0,256])
+            vect_features = np.concatenate((histB, np.concatenate((histG,histR),axis=None)),axis=None)
+        
+        elif algo_choice==2: # Histo HSV
+            algo_name = "HSV"
+            hsv = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+            histH = cv2.calcHist([hsv],[0],None,[256],[0,256])
+            histS = cv2.calcHist([hsv],[1],None,[256],[0,256])
+            histV = cv2.calcHist([hsv],[2],None,[256],[0,256])
+            vect_features = np.concatenate((histH, np.concatenate((histS,histV),axis=None)),axis=None)
+
         if algo_choice == 3:  # SIFT
+            algo_name = "SIFT"
             sift = cv2.SIFT_create()
             kps, vect_features = sift.detectAndCompute(img, None)
+        
+        elif algo_choice==4: #ORB
+            algo_name = "ORB"
+            orb = cv2.ORB_create()
+            # finding key points and descriptors of both images using detectAndCompute() function
+            key_point1,vect_features = orb.detectAndCompute(img,None)
+        
+        
         output_path = os.path.join(
             INPUT_DIR,
-            "Methode_" + str(algo_choice) + "_requete.txt"
+            "Methode_" + str(algo_name) + "_requete.txt"
         )
 
         np.savetxt(output_path, vect_features)
