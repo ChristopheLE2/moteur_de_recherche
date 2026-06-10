b1be466c47fcc40a988c2967298b327c0da782d7
ChristopheLE2 <van-minhchristophe.le@edu.esiee.fr>
2026-06-10 17:37:47 +0200

    fix interaction front et flask

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, render_template, redirect, url_for
 import os
 import time
+import json
 
 app = Flask(__name__)
 INPUT_DIR = "/input"
@@ -15,6 +16,26 @@ def upload():
     image_path = os.path.join(INPUT_DIR, "image.jpg")
     file.save(image_path)
 
+    return {"success": True}
+
+@app.route("/search", methods=["POST"])
+def search():
+    data = request.get_json()
+
+    config = {
+        "descriptors": data.get("descriptors", []),
+        "distance": data.get("distance"),
+        "nb_results": data.get("nb_results")
+    }
+
+    config_file = os.path.join(
+        INPUT_DIR,
+        "search_config.json"
+    )
+
+    with open(config_file, "w") as f:
+        json.dump(config, f)
+
     # déclenchement du job
     open(os.path.join(INPUT_DIR, "process.job"), "w").close()
 
@@ -26,12 +47,12 @@ def upload():
 
     while not os.path.exists(done_file):
         if time.time() - start > timeout:
-            return "timeout", 500
+            return {"success": False}, 500
         time.sleep(1)
 
     os.remove(done_file)
 
-    return redirect(url_for('home'))
+    return {"success": True}
 
 @app.route('/about')
 def about():
